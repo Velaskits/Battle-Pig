6da8a069d15405107006cf33395c96f9b625860c
AaronSatorresP <aaron_albertosatorres_@ceroca.cat>
2023-10-16 20:27:01 +0200

    Recuperando escenas acabadas

--- Assets/Scenes/CambioPantalla.cs
+++ Assets/Scenes/CambioPantalla.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class ControlScene : MonoBehaviour
+public class CambioPantalla : MonoBehaviour
 {
-
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
